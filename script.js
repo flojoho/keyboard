ceaaa0757652810cf7a79da7ca3b5fda31788c50
flojoho <44843303+flojoho@users.noreply.github.com>
2019-08-23 19:47:10 +0200

    leichtes wechseln zwischen tastenzuweisungen

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ function init() {
 
   const maxGain = 0.1;
 
-
   const oscillators = {};
   const noteNumberFromKeyCode = {}; //is not being used at the moment
-
 
   const audioCtx = new AudioContext();
 
@@ -15,31 +13,30 @@ function init() {
 
   const volumeSlider = document.getElementById('volumeSlider');
 
-  //left to right:
-  //const keyCodes = [49, 81, 65, 89, 50, 87, 83, 88, 51, 69, 68, 67, 52, 82, 70, 86, 53, 84, 71, 66, 54, 90, 72, 78, 55, 85, 74, 77, 56, 73, 75, 188, 57, 79, 76, 190, 48, 80, 192, 189, 219, 186, 222];
-  //right to left:
-  const keyCodes = [190, 192, 186, 221, 188, 76, 80, 219, 77, 75, 79, 48, 78, 74, 73, 57, 66, 72, 85, 56, 86, 71, 90, 55, 67, 70, 84, 54, 88, 68, 82, 53, 89, 83, 69, 52, 226, 65, 87, 51]
-  //left hand / right hand
-  //const keyCodes = [16, 226, 89, 88, 67, 86, 20, 65, 83, 68, 70, 71, 81, 87, 69, 82, 84, 90, 50, 51, 52, 53, 54, 55, 66, 78, 77, 188, 190, 189, 72, 74, 75, 76, 192, 222, 85, 73, 79, 80, 186, 187, 56, 57, 48, 219, 221, 8,]
-
+  const layout = 0;
+  let keyCodes;
+  if(layout === 0){
+    //right to left:
+    keyCodes = [190, 192, 186, 221, 188, 76, 80, 219, 77, 75, 79, 48, 78, 74, 73, 57, 66, 72, 85, 56, 86, 71, 90, 55, 67, 70, 84, 54, 88, 68, 82, 53, 89, 83, 69, 52, 226, 65, 87, 51]
+  } else if (layout === 1) {
+    //left to right:
+    keyCodes = [49, 81, 65, 89, 50, 87, 83, 88, 51, 69, 68, 67, 52, 82, 70, 86, 53, 84, 71, 66, 54, 90, 72, 78, 55, 85, 74, 77, 56, 73, 75, 188, 57, 79, 76, 190, 48, 80, 192, 189, 219, 186, 222];
+  } else if (layout === 2) {
+    //left hand / right hand
+   keyCodes = [16, 226, 89, 88, 67, 86, 20, 65, 83, 68, 70, 71, 81, 87, 69, 82, 84, 90, 50, 51, 52, 53, 54, 55, 66, 78, 77, 188, 190, 189, 72, 74, 75, 76, 192, 222, 85, 73, 79, 80, 186, 187, 56, 57, 48, 219, 221, 8,]
+  }
   keyCodes.forEach((cur, index) => {
     addOscillator(cur, index - 24); //minus 24 because otherwise the lowest note is 440Hz; todo: prettier solution for this
   });
 
-
-
-
+  function changeVolume(percentage){ //todo: should i use a number between 0 and 1 instead of percentages?
+    volume.gain.value = percentage / 100 * maxGain;
+  }
 
   if(localStorage.getItem('keyboard') !== null){ //todo: improve this isset-function
     const localData = localStorage.getItem('keyboard');
-    
     volumeSlider.value = localData;
     changeVolume(localData);
-  }
-
-
-  function changeVolume(percentage){ //todo: should i use a number between 0 and 1 instead of percentages?
-    volume.gain.value = percentage / 100 * maxGain;
   }
 
 
