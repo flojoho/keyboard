dca864d0c3f33d15b337219acf843920ee7ad486
flojoho <44843303+flojoho@users.noreply.github.com>
2019-09-08 00:30:41 +0200

    added new key layout

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function afterFirstUserAction() {
   const maxGain = 0.1;
 
   const oscillators = {};
-  const noteNumberFromKeyCode = {}; //is not being used at the moment
+  const noteNumberFromKeyCode = {};
 
   const audioCtx = new AudioContext();
 
@@ -13,26 +13,36 @@ function afterFirstUserAction() {
 
   const volumeSlider = document.getElementById('volumeSlider');
 
-  const keyboardRows = [
-    [220, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 219, 221, 8],
-    [9, 81, 87, 69, 82, 84, 90, 85, 73, 79, 80, 186, 187],
-    [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 192, 222, 191],
-    [226, 89, 88, 67, 86, 66, 78, 77, 188, 190, 189, 16]
-  ]
-
-  const activeLayout = 0;
-  const layouts = [
-    //right to left:
-    [190, 192, 186, 221, 188, 76, 80, 219, 77, 75, 79, 48, 78, 74, 73, 57, 66, 72, 85, 56, 86, 71, 90, 55, 67, 70, 84, 54, 88, 68, 82, 53, 89, 83, 69, 52, 226, 65, 87, 51],
-    //left to right:
-    [49, 81, 65, 89, 50, 87, 83, 88, 51, 69, 68, 67, 52, 82, 70, 86, 53, 84, 71, 66, 54, 90, 72, 78, 55, 85, 74, 77, 56, 73, 75, 188, 57, 79, 76, 190, 48, 80, 192, 189, 219, 186, 222],
-    //left hand / right hand
-    [16, 226, 89, 88, 67, 86, 20, 65, 83, 68, 70, 71, 81, 87, 69, 82, 84, 90, 50, 51, 52, 53, 54, 55, 66, 78, 77, 188, 190, 189, 72, 74, 75, 76, 192, 222, 85, 73, 79, 80, 186, 187, 56, 57, 48, 219, 221, 8,]
-  ];
-  keyCodes = layouts[activeLayout];
-  keyCodes.forEach((cur, index) => {
-    addOscillator(cur, index - 24); //minus 24 because otherwise the lowest note is 440Hz; todo: prettier solution for this
-  });
+  const intervalBetweenRows = 5;
+  setUpLayout(3);
+  function setUpLayout(layoutId){
+    if(layoutId === 3){
+      const keyboardRows = [
+        [226, 89, 88, 67, 86, 66, 78, 77, 188, 190, 189, 16],
+        [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 192, 222, 191],
+        [9, 81, 87, 69, 82, 84, 90, 85, 73, 79, 80, 186, 187],
+        [220, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 219, 221, 8]
+      ];
+      keyboardRows.map((row, rowNum) => {
+        row.map((keyCode, index) => {
+          addOscillator(keyCode, rowNum*intervalBetweenRows + index - 24); //minus 24 because otherwise the lowest note is 440Hz; todo: prettier solution for this
+        });
+      });
+      return;
+    }
+    const injectiveLayouts = [
+      //right to left:
+      [190, 192, 186, 221, 188, 76, 80, 219, 77, 75, 79, 48, 78, 74, 73, 57, 66, 72, 85, 56, 86, 71, 90, 55, 67, 70, 84, 54, 88, 68, 82, 53, 89, 83, 69, 52, 226, 65, 87, 51],
+      //left to right:
+      [49, 81, 65, 89, 50, 87, 83, 88, 51, 69, 68, 67, 52, 82, 70, 86, 53, 84, 71, 66, 54, 90, 72, 78, 55, 85, 74, 77, 56, 73, 75, 188, 57, 79, 76, 190, 48, 80, 192, 189, 219, 186, 222],
+      //left hand / right hand
+      [16, 226, 89, 88, 67, 86, 20, 65, 83, 68, 70, 71, 81, 87, 69, 82, 84, 90, 50, 51, 52, 53, 54, 55, 66, 78, 77, 188, 190, 189, 72, 74, 75, 76, 192, 222, 85, 73, 79, 80, 186, 187, 56, 57, 48, 219, 221, 8,]
+    ];
+    keyCodes = injectiveLayouts[layoutId];
+    keyCodes.forEach((cur, index) => {
+      addOscillator(cur, index - 24); //minus 24 because otherwise the lowest note is 440Hz; todo: prettier solution for this
+    });
+  }
 
   function changeVolume(percentage){ //todo: should i use a number between 0 and 1 instead of percentages?
     volume.gain.value = percentage / 100 * maxGain;
