4ce4536f5f40c3f0774e3516542cfdf720a45bfa
Florian Höllwerth <florian.hoellwerth@gmail.com>
2020-09-14 11:48:21 +0200

    some refactoring

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function afterFirstUserAction() {
   const maxGain = 0.1;
 
   const oscillators = {};
-  const noteNumberFromKeyCode = {};
 
   const audioCtx = new AudioContext();
 
@@ -14,52 +13,34 @@ function afterFirstUserAction() {
   const volumeSlider = document.getElementById('volumeSlider');
 
   const intervalBetweenRows = 5;
-  setUpLayout(3);
-  function setUpLayout(layoutId){
-    if(layoutId === 3){
-      const keyboardRows = [
-        [226, 89, 88, 67, 86, 66, 78, 77, 188, 190, 189, 16],
-        [20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 192, 222, 191],
-        [9, 81, 87, 69, 82, 84, 90, 85, 73, 79, 80, 186, 187],
-        [220, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 219, 221, 8]
-      ];
-      keyboardRows.map((row, rowNum) => {
-        row.map((keyCode, index) => {
-          addOscillator(keyCode, rowNum*intervalBetweenRows + index - 18);
-        });
-      });
-      return;
-    }
-    const injectiveLayouts = [
-      //right to left:
-      [190, 192, 186, 221, 188, 76, 80, 219, 77, 75, 79, 48, 78, 74, 73, 57, 66, 72, 85, 56, 86, 71, 90, 55, 67, 70, 84, 54, 88, 68, 82, 53, 89, 83, 69, 52, 226, 65, 87, 51],
-      //left to right:
-      [49, 81, 65, 89, 50, 87, 83, 88, 51, 69, 68, 67, 52, 82, 70, 86, 53, 84, 71, 66, 54, 90, 72, 78, 55, 85, 74, 77, 56, 73, 75, 188, 57, 79, 76, 190, 48, 80, 192, 189, 219, 186, 222],
-      //left hand / right hand
-      [16, 226, 89, 88, 67, 86, 20, 65, 83, 68, 70, 71, 81, 87, 69, 82, 84, 90, 50, 51, 52, 53, 54, 55, 66, 78, 77, 188, 190, 189, 72, 74, 75, 76, 192, 222, 85, 73, 79, 80, 186, 187, 56, 57, 48, 219, 221, 8,]
-    ];
-    keyCodes = injectiveLayouts[layoutId];
-    keyCodes.forEach((cur, index) => {
-      addOscillator(cur, index - 24); //minus 24 because otherwise the lowest note is 440Hz; todo: prettier solution for this
+  const keyboardRows = [
+    ["IntlBackslash", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight"],
+    ["CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Backslash"],
+    ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight"],
+    ["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"],
+  ];
+  keyboardRows.forEach((row, rowNum) => {
+    row.forEach((keyCode, index) => {
+      addOscillator(keyCode, rowNum*intervalBetweenRows + index - 18);
     });
-  }
+  });
 
-  function changeVolume(percentage){ //todo: should i use a number between 0 and 1 instead of percentages?
+  function changeVolume(percentage) { //todo: should i use a number between 0 and 1 instead of percentages?
     volume.gain.value = percentage / 100 * maxGain;
     localStorage.setItem('keyboard', percentage);
   }
 
-  if(localStorage.getItem('keyboard')){ //todo: improve this isset-function
+  if(localStorage.getItem('keyboard')) { //todo: improve this isset-function
     const localData = localStorage.getItem('keyboard');
     volumeSlider.value = localData;
     changeVolume(localData);
   }
 
-  function frequencyFromNoteNumber(noteNumber){
+  function frequencyFromNoteNumber(noteNumber) {
     return 440 * 2**(noteNumber / 12);
   }
 
-  function addOscillator(keyCode, noteNumber){
+  function addOscillator(keyCode, noteNumber) {
     oscillators[keyCode] = audioCtx.createOscillator();
     oscillators[keyCode].frequency.setValueAtTime(frequencyFromNoteNumber(noteNumber), audioCtx.currentTime);
     oscillators[keyCode].type = "square";
@@ -69,26 +50,26 @@ function afterFirstUserAction() {
 
   const keyPressed = [];
 
-  function keyGotPressed(keyCode){
+  function keyGotPressed(keyCode) {
     //if(noteNumberFromKeyCode[keyCode] !== undefined)
     
     //startNote(noteNumberFromKeyCode[e.keyCode]);
     if(oscillators[keyCode]) oscillators[keyCode].connect(volume);
   }
 
-  function keyGotReleased(keyCode){
+  function keyGotReleased(keyCode) {
     //stopNote(noteNumberFromKeyCode[e.keyCode]);
     if(oscillators[keyCode]) oscillators[keyCode].disconnect(volume);
   }
 
   //**************************** EVENT HANDLING ************************
   document.addEventListener('keydown', e => {
-    if([38, 40].includes(e.keyCode)) { // up/down keys
+    if(['ArrowUp', 'ArrowDown'].includes(e.code)) {
       const prevVolume = volumeSlider.value;
       let nextVolume;
-      if (e.keyCode === 38) {
+      if (e.code === 'ArrowUp') {
         nextVolume = parseInt(prevVolume) + 10;
-      } else if (e.keyCode === 40) {
+      } else if (e.code === 'ArrowDown') {
         nextVolume = prevVolume - 10;
       }
       
@@ -99,23 +80,23 @@ function afterFirstUserAction() {
       changeVolume(nextVolume);
       return;
     }
-    if([37, 39].includes(e.keyCode)) { //left/right keys
+    if(['ArrowLeft', 'ArrowRight'].includes(e.code)) {
       return;
     }
 
     audioCtx.resume(); //very ugly way of doing it
     
-    if(keyPressed[e.keyCode] !== true){
-      keyPressed[e.keyCode] = true;
-      keyGotPressed(e.keyCode);
+    if(keyPressed[e.code] !== true) {
+      keyPressed[e.code] = true;
+      keyGotPressed(e.code);
     }
   });
 
   document.addEventListener('keyup', e => {
-    if(keyPressed[e.keyCode] !== false){
-      keyPressed[e.keyCode] = false;
+    if(keyPressed[e.code] !== false){
+      keyPressed[e.code] = false;
       
-      keyGotReleased(e.keyCode);
+      keyGotReleased(e.code);
     }
   });
 
