d85cbe5ef8e760a11344ca675fab8b39d1e3c2ee
flojoho <hofkas.flo@gmx.at>
2023-01-13 21:23:45 +0100

    filled gaps

--- modules/ButtonGrid.js
+++ modules/ButtonGrid.js
@@ -2,26 +2,24 @@ import NoteButton, { diameter } from './NoteButton.js';
 
 const buttonGridContainer = document.getElementById('buttonGridContainer');
 
-export const spacing = 5;
-
 const render = () => {
   const divWidth = buttonGridContainer.offsetWidth;
   const divHeight = buttonGridContainer.offsetHeight;
 
-  const buttonsPerRow = Math.floor(divWidth / (diameter + spacing));
-  const buttonsPerColumn = Math.floor(divHeight / (diameter + spacing));
+  const buttonsPerRow = Math.floor(divWidth / diameter);
+  const buttonsPerColumn = Math.floor(divHeight / diameter);
   
-  const marginX = (divWidth - (buttonsPerRow * (diameter + spacing))) / 2;
-  const marginY = (divHeight - (buttonsPerColumn * (diameter + spacing))) / 2;
+  const marginX = (divWidth - (buttonsPerRow * diameter)) / 2;
+  const marginY = (divHeight - (buttonsPerColumn * diameter)) / 2;
 
   buttonGridContainer.textContent = '';
 
   for (let countY = 0; countY < buttonsPerColumn; countY++) {
     for(let countX = 0; countX < buttonsPerRow; countX++) {
-      const x = marginX + countX * (diameter + spacing);
-      const y = marginY + countY * (diameter + spacing);
+      const x = marginX + countX * diameter;
+      const y = marginY + countY * diameter;
 
-      const noteNumber = (Math.floor(buttonsPerColumn / 2)) - countY - 5 * (countX - Math.floor(buttonsPerRow / 2))
+      const noteNumber = countX - (Math.floor(buttonsPerRow / 2)) - 5 * (countY - Math.floor(buttonsPerColumn / 2));
       const noteButton = new NoteButton(noteNumber, x, y);
 
       noteButton.appendTo(buttonGridContainer);
--- modules/NoteButton.js
+++ modules/NoteButton.js
@@ -1,24 +1,31 @@
 import { Note } from './AudioHandler.js';
-import { spacing } from './ButtonGrid.js';
 
-export const diameter = 80;
+export const diameter = 85;
+const spacing = 5;
 
 class NoteButton {
   #div
   #noteNumber
-  #y
-  #yTouchStart
+  #x
+  #xTouchStart
   #note
   
   constructor(noteNumber, x, y) {
     this.#noteNumber = noteNumber;
-    this.#y = y;
+    this.#x = x;
+
+    const circle = document.createElement('div');
+    circle.classList.add('note-button');
+    circle.style.backgroundColor = `hsl(${ noteNumber / 12 * 360 }, 100%, 50%)`;
+    circle.style.width = `${ diameter - spacing }px`;
+    circle.style.height = `${ diameter - spacing }px`;
 
     this.#div = document.createElement('div');
-    this.#div.classList.add('note-button');
+    this.#div.appendChild(circle);
+    this.#div.style.position = `absolute`;
+    this.#div.style.display = `flex`;
     this.#div.style.width = `${ diameter }px`;
     this.#div.style.height = `${ diameter }px`;
-    this.#div.style.backgroundColor = `hsl(${ noteNumber / 12 * 360 }, 100%, 50%)`;
     this.#div.style.left = `${ x }px`;
     this.#div.style.top = `${ y }px`;
 
@@ -26,7 +33,7 @@ class NoteButton {
       e.preventDefault();
       if(this.#note) return;
 
-      this.#yTouchStart = e.targetTouches[0].clientY;
+      this.#xTouchStart = e.targetTouches[0].clientX;
       
       this.#note = new Note(this.#noteNumber);
       this.#note.start();
@@ -37,12 +44,12 @@ class NoteButton {
     this.#div.addEventListener('touchmove', e => {
       e.preventDefault();
 
-      const yMouse = e.targetTouches[0].clientY
+      const xMouse = e.targetTouches[0].clientX
 
-      const pitchChange = (this.#yTouchStart - yMouse) / (diameter + spacing);
+      const pitchChange = (xMouse - this.#xTouchStart) / diameter;
       this.#note.changePitch(pitchChange);
 
-      this.#div.style.top = `${ this.#y + (yMouse - this.#yTouchStart) }px`;
+      this.#div.style.left = `${ this.#x + (xMouse - this.#xTouchStart) }px`;
     });
 
     this.#div.addEventListener('touchend', e => {
@@ -53,7 +60,7 @@ class NoteButton {
 
       this.#div.classList.remove('note-button-active');
 
-      this.#div.style.top = `${ this.#y }px`;
+      this.#div.style.left = `${ this.#x }px`;
     });
   }
 
