ed061a765a2dbf8bdc0c2ec1d05c8e1666a47d31
flojoho <hofkas.flo@gmx.at>
2023-12-26 00:44:00 +0100

    fix keymapping bug

--- modules/KeyMapping.js
+++ modules/KeyMapping.js
@@ -1,10 +1,10 @@
 import settings from './settings.js';
 const intervalBetweenRows = 5;
 const keyboardRows = [
-    [undefined, "Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"],
+    [undefined, "IntlBackslash", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace"],
     [undefined, "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", undefined],
     [undefined, "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Backslash", undefined],
-    ["ShiftLeft", "IntlBackslash", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight", undefined, undefined]
+    ["ShiftLeft", "Backquote", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ShiftRight", undefined, undefined]
 ];
 const rightHandMapping = {};
 keyboardRows.reverse().forEach((row, rowNum) => {
